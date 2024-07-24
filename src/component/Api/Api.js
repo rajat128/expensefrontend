3d77c0cba97465abf86501013bd6e405afb2a9f6
rajat128 <155458603+rajat128@users.noreply.github.com>
2024-07-24 21:51:26 +0530

    Update Api.js

--- src/component/Api/Api.js
+++ src/component/Api/Api.js
@@ -1,6 +1,6 @@
 // const host = "https://expense-tracker-app-knl1.onrender.com";
 // const host = "http://localhost:8015";
-const host="https://backendexpense-1kzk.onrender.com"
+const host="https://backendexpense-k9jp.vercel.app"
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
